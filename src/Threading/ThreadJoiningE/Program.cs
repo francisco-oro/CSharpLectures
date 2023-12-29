ecb77b827f5f903bf22c773fed31c2b93c75196a
francisco-oro <francisco.oro@comunidad.unam.mx>
2023-12-29 16:11:44 -0600

    Threads

---
+++ src/Threading/ThreadJoiningE/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadJoiningE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread started");
+            Thread thread1 = new Thread(Thread1Function);
+            Thread thread2 = new Thread(Thread2Function);
+            thread1.Start();
+            thread2.Start();  
+
+            if (thread1.Join(1000))
+            {
+                Console.WriteLine("Thread1Function done");
+            }
+            else
+            {
+                Console.WriteLine("Thread1Function wasn't done within 1 sec");
+            }
+            thread2.Join();
+            Console.WriteLine("Thread2Function done");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (thread1.IsAlive)
+                {
+                    Console.WriteLine("Thread1 is still doing stuff");
+                    Thread.Sleep(300);
+                }
+                else
+                {
+                    Console.WriteLine("Thread1 completed");
+                }
+            }
+
+
+            Console.WriteLine("Main thread ended");
+        }
+
+        public static void Thread1Function()
+        {
+            Console.WriteLine("Thread1Function started");
+            Thread.Sleep(3000);
+            Console.WriteLine("Thread1Function coming back to caller");
+        }
+        public static void Thread2Function()
+        {
+            Console.WriteLine("Thread2Function started");
+        }
+    }
+}
---
+++ src/Threading/WPFTasksE/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTasksE
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached(
+                "Html",
+                typeof(string), typeof(MainWindow), new PropertyMetadata(OnHtmlChanged));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId}");
+                HttpClient webClient = new HttpClient();
+                try
+                {
+                    string html = webClient.GetStringAsync("http://ipv4.download.thinkbroadband.com/200MB.zip").Result;
+
+                } catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message); 
+                }
+                MyButton.Dispatcher.Invoke(() =>
+                {
+                    Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId} owns myButton");
+                    MyButton.Content = "Done";
+                });
+
+            });
+
+        }
+        private async void MyButton_Click2(object sender, RoutedEventArgs e)
+        {
+            string myHtml = "Bla";
+            Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId} before await task");
+
+            await Task.Run(async () =>
+            {
+                Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId} during await task");
+                HttpClient webClient = new HttpClient();
+                try
+                { 
+                    string html = webClient.GetStringAsync("https://phionira.com").Result;
+                    myHtml = html;
+                } catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message); 
+                }
+            });
+            Debug.WriteLine($"Thread Nr. {Thread.CurrentThread.ManagedThreadId} after await task");
+
+            MyButton.Content = "Done downloading";
+            myWebBrowser.SetValue(HtmlProperty, myHtml);
+        }
+
+        static void OnHtmlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser webBrowser = (WebBrowser)d;
+            if (webBrowser != null)
+            {
+                webBrowser.NavigateToString(e.NewValue as string);
+            }
+        }
+    }
+}
