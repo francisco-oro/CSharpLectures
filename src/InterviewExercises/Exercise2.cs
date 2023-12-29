226a740dcd04e345ab6f06932bad7b7f0d249a35
francisco-oro <francisco.oro@comunidad.unam.mx>
2023-12-28 22:40:20 -0600

    coding exercises

---
+++ src/InterviewExercises/Exercise2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewExercises
+{
+    using System;
+    using System.Collections.Generic;
+
+    namespace Coding.Exercise
+    {
+        internal class Exercise2
+        {
+            public static int SumOfTwo(int[] nums, int SumToFind)
+            {
+                Dictionary<int, int> dic = new Dictionary<int, int>();
+                int result = 0;
+
+                foreach (int value in nums)
+                {
+                    if (dic.ContainsKey(SumToFind - value) && dic[SumToFind - value] > 0)
+                    {
+
+                        dic[SumToFind - value] -= 1;
+
+                        result++;
+                        continue;
+                    }
+                    if (dic.ContainsKey(value))
+                    {
+                        dic[value] += 1;
+                    }
+                    else
+                    {
+                        dic.Add(value, 1);
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ src/InterviewExercises/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewExercises
+{
+
+    public class Exercise
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(NotHungryCats("F O~ O~ ~O O~"));
+            Console.ReadKey();
+        }
+        public static int NotHungryCats(string kitchen)
+        {
+            int count = 0;
+            try
+            {
+                int UserPosition = kitchen.IndexOf('F');
+                kitchen = kitchen.Replace(" ", "");
+                for (int i = 0; i < UserPosition; i += 2)
+                {
+                    if (GetDirection(kitchen.Substring(i, 2)).Equals(direction.left))
+                        count++;
+                }
+                for (int i = UserPosition + 1; i < kitchen.Length; i += 2)
+                {
+                    if (GetDirection(kitchen.Substring(i, 2)).Equals(direction.right))
+                        count++;
+                }
+            } catch (Exception ex) {  Console.WriteLine(ex); }
+            return count;
+        }
+
+        private static direction GetDirection(string cat)
+        {
+            if (cat.Equals("~O"))
+                return direction.right;
+            if(cat.Equals("O~"))
+                return direction.left;
+            return direction.up;        
+        }
+        private enum direction
+        {
+            left,
+            right,
+            up
+        }
+
+    }
+}
